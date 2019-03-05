17e5f3e06f7f6a8b31163745b69a11683aad9d31
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-04 18:28:46 -0800

    reversed the change

--- application.py
+++ application.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-#def hello():
-#    return "Hello, this is Nomad Cycling Club official website. site is currently under constructions..."
+def hello():
+    return "Hello, this is Nomad Cycling Club official website. site is currently under constructions..."
