c09c66faa59f11328428029195c4872fc40ed6a7
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 13:36:46 -0700

    added azure word

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, this is Nomad Cycling Club official website. site is currently under constructions.."
+    return "Hello, this is Nomad Cycling Club official website running on Azure. site is currently under constructions.."
