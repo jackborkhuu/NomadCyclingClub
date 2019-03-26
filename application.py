cffd5f7dcaa826d99d54d2df993d153d71a788e3
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 16:41:28 -0700

    test code

--- application.py
+++ application.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, this is Nomad Cycling Club official website running on Azure. Site is currently under constructions.."
+# def hello():
+  #  return "Hello, this is Nomad Cycling Club official website running on Azure. Site is currently under constructions.."
+def post(self):
+        username = self.request.get("my_name")
+        welcome_string = """<html><body>
+                          Hi there, {}!
+                          </body></html>""".format(username)
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write(welcome_string)
