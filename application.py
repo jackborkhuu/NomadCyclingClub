18dc02eb2ae5fb4cb7590f5eb66d7c1dee8bbb6c
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 16:53:15 -0700

    rewrite1

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
- #   return "Hello, this is Nomad Cycling Club official website running on Azure. Site is currently under constructions.."
     name = request.args.get("name", "World")
     return f'Hello, {name}!'
