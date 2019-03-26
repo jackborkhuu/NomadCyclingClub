5a1ad22ef3bfe70a34a5ace70afd6aeb14c46a23
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 16:58:32 -0700

    sample

--- application.py
+++ application.py
@@ -1,8 +1,30 @@
-from flask import Flask, request
+import webapp2
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {name}!'
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write("""
+          <html>
+            <head><title>Enter your name...</title></head>
+            <body>
+              <form action="/welcome" method="post">
+                <input type="text" name="my_name"><br>
+                <input type="submit" value="Sign In">
+              </form>
+            </body>
+            </html>""")
+
+
+class Greeting(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get("my_name")
+        welcome_string = """<html><body>
+                          Hi there, {}!
+                          </body></html>""".format(username)
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write(welcome_string)
+
+
+routes = [('/', MainPage), ('/welcome', Greeting)]
+my_app = webapp2.WSGIApplication(routes, debug=True)
