8ff7efee45426c1bb609f93f393479989ec5c8bb
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 17:21:12 -0700

    rewrite

--- application.py
+++ application.py
@@ -1,30 +1,15 @@
-import webapp
-
-
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.headers["Content-Type"] = "text/html"
-        self.response.write("""
-          <html>
-            <head><title>Enter your name...</title></head>
-            <body>
-              <form action="/welcome" method="post">
-                <input type="text" name="my_name"><br>
-                <input type="submit" value="Sign In">
-              </form>
-            </body>
-            </html>""")
-
-
-class Greeting(webapp.RequestHandler):
-    def post(self):
-        username = self.request.get("my_name")
-        welcome_string = """<html><body>
-                          Hi there, {}!
-                          </body></html>""".format(username)
-        self.response.headers["Content-Type"] = "text/html"
-        self.response.write(welcome_string)
-
-
-routes = [('/', MainPage), ('/welcome', Greeting)]
-my_app = webapp.WSGIApplication(routes, debug=True)
+from flask import Flask, flash, redirect, render_template, request, session, abort
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+return "Flask App!"
+ 
+@app.route("/hello/<string:name>/")
+def hello(name):
+return render_template(
+'test.html',name=name)</string:name>
+ 
+if __name__ == "__main__":
+app.run()
