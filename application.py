9c3a75dc7fe323437bfd3c561343124590f24199
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 17:54:58 -0700

    restore

--- application.py
+++ application.py
@@ -3,7 +3,4 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-return "Hello, this is Nomad Cyling Club Web Site running on Azure web services!"
- 
-if __name__ == "__main__":
-app.run()
+    return "Hello, this is Nomad Cyling Club Web Site running on Azure web services!"
