6e414e17be45540d84e5c9c2b712ae3fc83cbb56
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 17:50:28 -0700

    resore

--- application.py
+++ application.py
@@ -1,37 +1,9 @@
-# pythonspot.com
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
- 
-# App config.
-DEBUG = True
+from flask import Flask
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
  
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
- 
- 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def hello():
-    form = ReusableForm(request.form)
- 
-    print form.errors
-    if request.method == 'POST':
-        name=request.form['name']
-        password=request.form['password']
-        email=request.form['email']
-        print name, " ", email, " ", password
- 
-        if form.validate():
-            # Save the comment here.
-            flash('Thanks for registration ' + name)
-        else:
-            flash('Error: All the form fields are required. ')
- 
-    return render_template('hello.html', form=form)
+return "Hello, this is Nomad Cyling Club Web Site running on Azure web services!"
  
 if __name__ == "__main__":
-    app.run()
+app.run()
