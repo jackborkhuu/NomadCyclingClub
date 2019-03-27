7b561a248d4645724e3a885978902b04e57088f6
Jack Borkhuu <47874063+jackborkhuu@users.noreply.github.com>
2019-03-26 17:56:13 -0700

    spelling correction

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-    return "Hello, this is Nomad Cyling Club Web Site running on Azure web services!"
+    return "Hello, this is Nomad Cycling Club Web Site running on Azure web services!"
